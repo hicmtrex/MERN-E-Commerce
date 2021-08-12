87b8b7552de5bc07d72dfe1d6d0d451e49931d93
hicm <hicmrex@gmail.com>
2021-08-13 00:14:00 +0100

    fixex

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,6 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
-
-app.use(express.static('client/build'));
-app.get('*', (req, res) => res.sendFile(`${__dirname}/frontend/build/index.html`));
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on Port ${PORT}`))

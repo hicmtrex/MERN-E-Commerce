25a796915a1f07dbef441d7f812b1831d243f16f
hicm <hicmrex@gmail.com>
2021-08-15 04:49:29 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+const app = express();
+
+//Routes Const 
 const productRoutes = require('./routes/productRoutes');
 
 connectDB();
 
-const app = express();
-app.use(express.json());
 
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }))
+
+//Routes
 app.use('/api/products', productRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build' , 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api running')
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
